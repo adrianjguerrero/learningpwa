e69fb18a0ce5cb836acd3336a6c0bc7ca97e2a19
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-07-15 21:15:05 -0400

    "save"

--- twittor-2/server/push.js
+++ twittor-2/server/push.js
@@ -1,7 +1,11 @@
+const fs = require('fs')
+
 const urlbase64 = require('urlsafe-base64')
 const vapid = require('./vapid.json')
 
-const suscripciones = []
+
+// simulacion de db lol
+const suscripciones = require('./subs-db.json')
 
 // es mejor encodear las vainas, asi van mas seguro
 module.exports.getKey = () => urlbase64.decode( vapid.publicKey)
@@ -9,6 +13,9 @@ module.exports.getKey = () => urlbase64.decode( vapid.publicKey)
 module.exports.addSubscription = (subscription) =>{
     
     suscripciones.push(subscription)
+    
+    // guardaremos en un archivo pos puro por simular
+    fs.writeFileSync(`${__dirname}/subs-db.json`, JSON.stringify(suscripciones))
 
     console.log(suscripciones);
 
